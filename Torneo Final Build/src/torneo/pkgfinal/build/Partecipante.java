e1bcda5464b030e858299aad10c58287d22b81fa
JohanssonXXII <69969647+JohanssonXXII@users.noreply.github.com>
2020-11-28 19:36:51 +0100

    Code update
    Creata la classe partecipante e creato il main

---
+++ Torneo Final Build/src/torneo/pkgfinal/build/Partecipante.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package torneo.pkgfinal.build;
+
+/**
+ *
+ * @author Aimen
+ */
+public class Partecipante {
+	private String fName;
+	private String sName;
+	
+	public Partecipante(String fName, String sName) {
+		super();
+		this.fName = fName;
+		this.sName = sName;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getsName() {
+		return sName;
+	}
+	public void setsName(String sName) {
+		this.sName = sName;
+	}
+	@Override
+	public String toString() {
+		return this.fName +" " +this.sName;
+	}
+	
+
+}
