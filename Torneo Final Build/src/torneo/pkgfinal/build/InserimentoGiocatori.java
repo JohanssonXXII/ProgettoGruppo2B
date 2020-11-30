0776aef00b2b102dc1e55302202c126de7740d9d
AlessioVa <70761534+AlessioVa@users.noreply.github.com>
2020-11-30 15:40:21 +0100

    Update InserimentoGiocatori.java

--- Torneo Final Build/src/torneo/pkgfinal/build/InserimentoGiocatori.java
+++ Torneo Final Build/src/torneo/pkgfinal/build/InserimentoGiocatori.java
@@ -4,11 +4,36 @@
  * and open the template in the editor.
  */
 package torneo.pkgfinal.build;
-
+import java.util.ArrayList;
 /**
  *
  * @author Aimen
  */
 public class InserimentoGiocatori {
+    
+    protected ArrayList<PARTECIPANTE> list;
+	
+	public InserimentoGiocatori() {
+		super();
+		this.list = new ArrayList<PARTECIPANTE>();
+	}
+	
+
+	public ArrayList<PARTECIPANTE> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<PARTECIPANTE> list) {
+		this.list = list;
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+
     
 }
