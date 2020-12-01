0d2c9b94420333d6ddcca9c9c922e23aa118872f
nicholas1002 <pancaldinicholas@gmail.com>
2020-12-01 09:43:27 +0100

    Create Tabellone.java

---
+++ Torneo Final Build/src/torneo/pkgfinal/build/Tabellone.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.*;
+public class Tabellone {
+	protected static int count = 0;
+	
+	public static InserimentoGiocatori Tournament(InserimentoGiocatori obj) {
+		
+		ArrayList<Partecipante> list = obj.getList();
+		InserimentoGiocatori toReturn = new InserimentoGiocatori();
+		Scanner input = new Scanner(System.in);
+		int length = list.size();
+		int j = (int) Math.floor((int)(Math.log(length) / Math.log(2)));
+		j = (int) Math.pow(2, j);
+		j = j - 1;
+		int i =0;
+		int s = j;
