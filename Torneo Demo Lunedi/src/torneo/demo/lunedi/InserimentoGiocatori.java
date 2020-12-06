830e502fc6dd197f0e2ccf539c99a7ed0fe38e7b
Prof. Missiroli (home) <prof.missiroli@gmail.com>
2020-12-06 14:14:17 +0100

    Commenti prof

--- Torneo Demo Lunedi/src/torneo/demo/lunedi/InserimentoGiocatori.java
+++ Torneo Demo Lunedi/src/torneo/demo/lunedi/InserimentoGiocatori.java
@@ -10,6 +10,24 @@ import java.util.ArrayList;
  *
  * @author Aimen
  */
+
+/**
+ * Inserimento giocatori è un brutto esempio di strutturazione di classi.
+ * Il fatto stesso che abbia il verbo nel nome indica che non è "veramente" un oggetto
+ * L'oggetto si dovrebbe chiamare "Giocatori" e avare al suo interno un metodo "inserimento"
+ * così lo tiri via dal main.
+ * Lo stesso vale per il salvataggio e il recupero dei dati.
+ *
+ * Cosa analoga protreste fare per i Partecipanti
+ *
+ * In generale, state pensando ancora in modo C++. L'oggetto sono i dati, e l'uso dei dati si fa nelle
+ * procedure. Negli oggetti, ogni oggetto gestisce tutto quello che lo riguarda, gli oggetti superiori
+ * (es: Tabella) gestiscono le INTERAZIONI tra gli oggetti.
+ *
+ * Provate a tener conto di questo, e riorganizzate anche l'UML in tal senso.
+ *
+ */
+
 public class InserimentoGiocatori {
     
     protected ArrayList<Partecipante> list;
